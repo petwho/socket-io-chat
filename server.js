d490ac0d20d02fb910ea78927f87520ef15a92a5
trankhanhsvn <trankhanhsvn@gmail.com>
2013-12-17 12:09:47 +0700

    - Implement chat - Separate users into rooms

--- server.js
+++ server.js
@@ -18,9 +18,23 @@ function handler(req, res) {
 io.sockets.on('connection', function (socket) {
   socket.on('clientMessage', function(content) {
     socket.emit('serverMessage', 'You said: ' + content);
-    socket.broadcast.emit('serverMessage', socket.id + ' said: ' +
-      content);
+    // socket.broadcast.emit('serverMessage', socket.id + ' said: ' + content);
+    socket.get('username', function(err, username) {
+      if (! username) {
+        username = socket.id;
+      }
+      socket.get('room', function(err, room) {
+        if (err) { throw err; }
+        var broadcast = socket.broadcast;
+        var message = content;
+        if (room) {
+          broadcast.to(room);
+        }
+        broadcast.emit('serverMessage', username + ' said: ' + message);
+      });
+    });
   });
+
 
   socket.on('login', function(username) {
     socket.set('username', username, function(err) {
@@ -33,6 +47,53 @@ io.sockets.on('connection', function (socket) {
       socket.broadcast.emit('serverMessage', 'User ' + username + ' logged in');
     });
   });
+
+  socket.on('disconnect', function() {
+    socket.get('username', function(err, username) {
+      if(!username) {
+        username = socket.id;
+      }
+
+      socket.broadcast.emit('serverMessage', 'User ' + username + ' disconnnected');
+    });
+  });
+
+  socket.on('join', function(room) {
+    // Get previous connection's room
+    socket.get('room', function(err, oldRoom) {
+
+      if (err) { throw err; }
+
+      // Set new connection's room
+      socket.set('room', room, function(err) {
+        if (err) { throw err; }
+
+        socket.join(room);
+
+        // Leave previous connections'room IF there is any
+        if (oldRoom) {
+          socket.leave(oldRoom);
+        }
+
+        socket.get('username', function(err, username) {
+          if (! username) {
+            username = socket.id;
+          }
+        });
+
+        socket.emit('serverMessage', 'You joined room ' + room);
+
+        socket.get('username', function(err, username) {
+          if (! username) {
+            username = socket.id;
+          }
+
+          socket.broadcast.to(room).emit('serverMessage', 'User ' + username + ' joined this room');
+        });
+      });
+    });
+  });
+
 
   socket.emit('login');
 });
